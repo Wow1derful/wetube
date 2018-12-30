3c57d15db4d29c96d8d96d46f7c24f0f347b210f
Golden, Jang <wow1derful@gmail.com>
2018-12-30 23:46:48 +0900

    middleware doing

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from"helmet";
+import cookieParser from"cookie-parser";
+import bodyParser from"body-parser";
 
 const app = express();
 
@@ -7,9 +11,19 @@ const PORT = 4000;
 const handleListening = () => 
     console.log('Listening on: http://localhost:${PORT}');
 
-const handleHome = (req, res) => res.send("Hello from home");
+const handleHome = (req, res) => res.send("Hello from my ass");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.use(cookieParser());
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(helmet());
+
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 
