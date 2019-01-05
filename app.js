07dd6750ff75cbbe2ae96c4b5dcc7f2695eeba4e
Golden, Jang <wow1derful@gmail.com>
2019-01-06 02:16:25 +0900

    pug add

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ import userRouter from"./routers/userRouter";
 import videoRouter from"./routers/vidoeRouter";
 import globalRouter from"./routers/globalRouter";
 import routes from"./routes";
-
-
 const app = express();
 
+app.set("view engine","pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
