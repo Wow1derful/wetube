67b6b39382d6a275228a62cb3e23c95bbf8dc88a
Golden, Jang <wow1derful@gmail.com>
2019-01-06 23:41:36 +0900

    pug add

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ import morgan from "morgan";
 import helmet from"helmet";
 import cookieParser from"cookie-parser";
 import bodyParser from"body-parser";
+import { localsMiddeleware } from "./middleWares";
+import routes from"./routes";
 import userRouter from"./routers/userRouter";
 import videoRouter from"./routers/vidoeRouter";
 import globalRouter from"./routers/globalRouter";
-import routes from"./routes";
 const app = express();
 
+app.use(helmet());
 app.set("view engine","pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(helmet());
 app.use(morgan("dev"));
+app.use(localsMiddeleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
